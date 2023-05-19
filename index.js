2d86339b1096e0c58bf70725aeff4ed774b37b88
weirdRafi <mazharulrafi@gmail.com>
2023-05-19 22:36:05 +0600

    subcategory updated

--- index.js
+++ index.js
@@ -19,85 +19,98 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const toyCollection = client.db('toyDB').collection('services')
+        const toyCollection = client.db('toyDB').collection('services')
+
+        // Post item to database 
+
+        app.post('/upload', async (req, res) => {
+            const data = req.body;
+            const result = await toyCollection.insertOne(data);
+            res.send(result);
+        })
+
+        // Get item by subcategory
+
+        app.get('/alltoys/:text', async (req, res) => {
+
+            if (req.params.text === 'classicCars' || req.params.text === 'racingCars' || req.params.text === 'fireTrucks') {
+                const result = await toyCollection.find({ subcategory: req.params.text }).toArray();
+                return res.send(result);
+            }
+            const result = await toyCollection.find({}).toArray();
+            res.send(result);
+
+        })
+
+        // Get Single item by Email 
+
+        app.get('/alltoys', async (req, res) => {
+            let query = {}
+            if (req.query?.sellerEmail) {
+                query = { sellerEmail: req.query.sellerEmail }
+            }
+            console.log(query);
+            const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // Get single item by Id 
+
+        app.get('/singletoy/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const data = await toyCollection.findOne(filter);
+            res.send(data);
+        })
+
+        // Update Item 
+
+        app.patch('/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body
+            const filter = { _id: new ObjectId(id) }
+            const doc = {
+                $set: { ...updatedData }
+            }
+            const result = await toyCollection.updateOne(filter, doc)
+            res.send(result)
+        })
 
 
-app.post('/upload', async(req,res)=>{
-    const data = req.body;
-    const result = await toyCollection.insertOne(data);
-    res.send(result);
-})
+        // Delete Item 
 
-// app.get('/alltoys', async(req,res)=>{
-//     const toys = toyCollection.find();
-//     const result = await toys.toArray();
-//     res.send(result);
-// })
+        app.delete('/sigletoy/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(filter);
+            res.send(result);
+        })
 
 
-app.get('/alltoys', async(req, res) => {
-    let query ={}
-    if(req.query?.sellerEmail){
-        query = {sellerEmail: req.query.sellerEmail}
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-    console.log(query);
-    const result = await toyCollection.find(query).toArray();
-    res.send(result);
-})
-
-
-app.get('/singletoy/:id', async(req, res)=>{
-    const id = req.params.id;
-    const filter= {_id: new ObjectId(id)}
-    const data= await toyCollection.findOne(filter);
-    res.send(data);
-})
-
-
-
-app.patch('/update/:id', async(req, res)=>{
-    const id=req.params.id;
-    const updatedData = req.body
-    const filter = {_id: new ObjectId(id)}
-    const doc ={
-        $set: {...updatedData}
-    }
-    const result = await toyCollection.updateOne(filter, doc)
-    res.send(result)
-})
-
-app.delete('/sigletoy/:id', async (req, res) => {
-    const id= req.params.id;
-    const filter= {_id: new ObjectId(id)}
-    const result = await toyCollection.deleteOne(filter);
-    res.send(result);
-})
-
-
-
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
 }
 run().catch(console.dir);
 
@@ -106,6 +119,6 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Toy is running')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`ToyPlanet is running on port ${port}`);
 })
